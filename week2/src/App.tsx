c17dc1a1d7f9ddce6d85f4dc1557209e545eb183
kxxnayun <nayun2179@gmail.com>
2026-09-21 10:55:41 +0900

    feat: 영화 정보 카드 작성하기

--- week2/src/App.tsx
+++ week2/src/App.tsx
@@ -1,35 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const movieTitle = "오디세이";
+  const genre = "모험";
+  const releaseDate = "2026-09-21";
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{movieTitle}</div>
+      <div>장르: {genre}</div>
+      <div>개봉일: {releaseDate}</div>
     </>
   );
 }
-
-export default App;
